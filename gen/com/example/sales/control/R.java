9c9dffd0cefd2a5616e14563d68847d08968a3f9
Simone Amorim <simoneas02@hotmail.com>
2016-05-21 23:13:09 -0300

    Added checkbox conected for login

--- gen/com/example/sales/control/R.java
+++ gen/com/example/sales/control/R.java
@@ -25,23 +25,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int login_btnLogin=0x7f080003;
-        public static final int login_ckbConectado=0x7f080002;
-        public static final int login_edtPassword=0x7f080001;
-        public static final int login_edtUser=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int login_btnLogin=0x7f080006;
+        public static final int login_ckbConected=0x7f080005;
+        public static final int login_edtPassword=0x7f080004;
+        public static final int login_edtUser=0x7f080003;
+        public static final int user_edtLogin=0x7f080001;
+        public static final int user_edtName=0x7f080000;
+        public static final int user_edtPassword=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_cad_user=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
-        public static final int login=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int cad_user=0x7f070000;
+        public static final int login=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f05000b;
         public static final int login_btnLogin=0x7f050005;
         public static final int login_ckbConected=0x7f050006;
         public static final int login_txtPassword=0x7f050004;
@@ -52,7 +58,16 @@ public final class R {
         /**  Validações 
          */
         public static final int login_validateUser=0x7f050007;
+        /**  Mensages 
+         */
+        public static final int msg_login_wrong=0x7f050009;
+        public static final int title_activity_cad_user=0x7f05000a;
         public static final int title_activity_login=0x7f050002;
+        public static final int user_txtLogin=0x7f05000d;
+        /**  Users 
+         */
+        public static final int user_txtName=0x7f05000c;
+        public static final int user_txtPassword=0x7f05000e;
     }
     public static final class style {
         /** 
@@ -81,5 +96,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int NameOfFields=0x7f060002;
     }
 }
--- src/com/example/sales/control/LoginActivity.java
+++ src/com/example/sales/control/LoginActivity.java
@@ -2,16 +2,23 @@ package com.example.sales.control;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import dao.UserDao;
+import util.Mensage;
 
 public class LoginActivity extends Activity {
 	private EditText edtUser, edtPassword;
 	private UserDao helper;
+	private CheckBox ckbConected;
+	
+	private static final String MANTER_CONECTADO = "manter_conectado";
+	private static final String PREFERENCE_NAME = "LoginActivityPreferences";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +27,16 @@ public class LoginActivity extends Activity {
 		
 		edtUser = (EditText)findViewById(R.id.login_edtUser);
 		edtPassword = (EditText)findViewById(R.id.login_edtPassword);
+		ckbConected = (CheckBox)findViewById(R.id.login_ckbConected);
 		
 		helper = new UserDao(this);
+		
+		SharedPreferences preferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+		boolean conectado = preferences.getBoolean(MANTER_CONECTADO, false);
+		
+		if (conectado) {
+			CallMainActivity();
+		}
 	}
 	
 	public void login(View view) {
@@ -43,10 +58,24 @@ public class LoginActivity extends Activity {
 		if (validate) {
 			//login
 			if(helper.login(user, password)){
-				startActivity(new Intent(this, MainActivity.class));
-				finish();
-			};
+				if (ckbConected.isChecked()) {
+					SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+					SharedPreferences.Editor editor = sharedPreferences.edit();
+					
+					editor.putBoolean(MANTER_CONECTADO, true);
+					editor.commit();
+				}
+				CallMainActivity();
+			}else{
+				//Error Mensage
+				Mensage.Msg(this, getString(R.string.msg_login_wrong));
+			}
 		}
+	}
+	
+	private void CallMainActivity() {
+		startActivity(new Intent(this, MainActivity.class));
+		finish();
 	}
 
 
